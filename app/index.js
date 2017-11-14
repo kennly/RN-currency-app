8a085c9d73cdc3de5fb06583fd9db8734825803e
Kenneth Au <kennethau82@gmail.com>
2017-11-14 21:29:30 +1000

    Start Reworking on this app

--- app/index.js
+++ app/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import Themes from './screens/Themes';
-import EStyleSheet from 'react-native-extended-stylesheet';
+import React from "react";
+import Home from "./screens/Home";
+import EStyleSheet from "react-native-extended-stylesheet";
 
 EStyleSheet.build({
-  $primaryBlue: '#4F6D7A',
-  $primaryOrage: '#D57A66',
-  $primaryGreen: '#00DB90',
-  $primaryPurple: '#9E768F',
+  $primaryBlue: "#4F6D7A",
+  $primaryOrage: "#D57A66",
+  $primaryGreen: "#00DB90",
+  $primaryPurple: "#9E768F",
 
+  $white: "#fff",
+  $border: "#E2E2E2",
+  $inputText: "#797979",
+  $lightGray: "#e9e9e9",
+  $darkText: "#343434"
+});
 
-
-  $white: '#fff',
-  $border: '#E2E2E2',
-  $inputText: '#797979',
-  $lightGray: '#e9e9e9',
-  $darkText: '#343434'
-})
-
-export default () => <Themes />;
+export default () => <Home />;
